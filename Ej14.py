6427f11c3731e229c51bbea9f120b6215e5e4bed
DavidBM7 <https://github.com/DavidBM7/Tasca10.git>
2023-11-14 13:31:01 +0100

    Ej14.py

--- Ej14.py
+++ Ej14.py
@@ -1,1 +1,13 @@
-def gran_de_tres(num1,)
+def gran_de_tres(num1, num2, num3):
+    if num1>num2>num3:
+        print(num1)
+    elif num2>num1>num3:
+        print(num2)
+    else:
+        print(num3)
+
+#Programari principal
+num1= int(input("Introdueix el primer nombre: "))
+num2= int(input("Introdueix el segon nombre: "))
+num3= int(input("Introdueix el tercer nombre: "))
+gran_de_tres(num1, num2, num3)
