be6da240343659603ba04756c606a297939115b0
DavidBM7 <https://github.com/DavidBM7/Tasca10.git>
2023-12-13 09:28:46 +0100

    comentaris un par

--- Ej57.py
+++ Ej57.py
@@ -1,9 +1,12 @@
 def mostrar(i):
-	b=[]
-	for e in range(i,0,-1):
-		b.append(e)
-	print(' '.join(map(str,b)))
-#PPrincipal
-x = int(input("Introdueixi un número petit: "))
-for i in range(x,0,-1):
-	mostrar(i)
+    # Función para mostrar una lista de números en orden descendente
+    b = []  # Inicializa una lista vacía para almacenar los números
+    for e in range(i, 0, -1):
+        b.append(e)  # Agrega cada número al final de la lista
+    print(' '.join(map(str, b)))  # Imprime la lista de números como una cadena
+
+# Parte principal del programa
+x = int(input("Introdueixi un número petit: "))  # Solicita al usuario un número
+for i in range(x, 0, -1):
+    mostrar(i)  # Llama a la función mostrar para imprimir listas descendentes
+
